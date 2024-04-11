d366e5845fb8d8c3500542084ddc14ade9e94a08
blackoceans <blackoceans@naver.com>
2024-04-11 11:44:16 +0900

    0411

--- project06/src/components/WishFooter.vue
+++ project06/src/components/WishFooter.vue
@@ -1,19 +1,25 @@
 <!-- WishFooter.vue -->
 <template lang="html">
-    <div class="clearAllcontainer">
-        <span class="clearAllBtn" @click="clearWish">Clear All</span>
+    <div class="buttonContainer">
+        <div class="clearAllcontainer">
+            <span class="clearAllBtn" @click="clearWish">모든 일정 삭제</span>
+        </div>
     </div>
 </template>
 <script>
     export default {
         methods: {
             clearWish(){
-                this.$emit('removeAll');  //상위에 있는 remeveall사용하는 함수
+                const isAllRemover = confirm("정말 모든 일정을 삭제하시겠습니까?");
+                if(isAllRemover) {
+                        this.$emit('removeAllOfChild');
+                }
             }
         }
     }
 </script>
 <style lang="css" scoped>
+    .buttonContainer{margin:0 auto; cursor:pointer;}
     .clearAllcontainer{width:7.5em; height: 50px; line-height:50px; background:linear-gradient(to right, #ff4c4d, #b30000); border-radius:5px; margin: 25px auto 0px;}
     .clearAllBtn{font-weight:bold; text-transform:uppercase; letter-spacing: -1px; color: white; display:block; cursor:pointer}
 
